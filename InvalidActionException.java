8be1e56a8e96aa0ab44378eba8e74bbeace0d9e5
AmirJ <64044992+amirjimdaniels@users.noreply.github.com>
2023-09-30 10:26:43 -0400

    Update InvalidActionException.java

---
+++ InvalidActionException.java
@@ -0,0 +1,11 @@
+package cs211.puz;
+
+public class InvalidActionException extends Exception {
+
+	public InvalidActionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
