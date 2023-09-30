9b968bff5b566b5231e2a005b47ca3e1da1a1f4f
AmirJ <64044992+amirjimdaniels@users.noreply.github.com>
2023-09-30 10:27:07 -0400

    Update PqFrontier.java

---
+++ PqFrontier.java
@@ -0,0 +1,43 @@
+package cs211.puz;
+
+/**
+ * Student uses a priority queue with the appropriate comparator 
+ * in order to give priority needed for informed searches.
+ * 
+ * @author 
+ *
+ */
+public class PqFrontier implements Frontier {
+
+	@Override
+	public void addNode(TreeNode n) {
+		// TODO replace this stub with your code
+	}
+
+	@Override
+	public TreeNode removeNode() {
+		// TODO replace this stub with your code
+		return null;
+	}
+
+	@Override
+	public boolean containsState(PuzState state) {
+		// TODO replace this stub with your code
+		return false;
+	}
+
+	@Override
+	public TreeNode findNode(PuzState state) {
+		// TODO replace this stub with your code
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO replace this stub with your code
+		return false;
+	}
+
+
+
+}
